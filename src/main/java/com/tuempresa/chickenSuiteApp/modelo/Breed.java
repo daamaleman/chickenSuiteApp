22b7449d6912b70ad7ae7e8bbae1e213c503a39c
eliasmarin2390 <eamarin@uamv.edu.ni>
2025-11-13 20:26:47 -0600

    Añado al modelo las entidades Breed y Purpose al proyecto

---
+++ src/main/java/com/tuempresa/chickenSuiteApp/modelo/Breed.java
@@ -0,0 +1,31 @@
+import javax.persistence.*;
+
+import com.tuempresa.chickenSuiteApp.modelo.Purpose;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Breed {
+
+    @Id
+    @Hidden
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=50)
+    @Required
+    String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @DescriptionsList // se muestra como combo
+    Species species;
+
+    @Enumerated(EnumType.STRING)
+    @Required
+    Purpose purpose;   // MEAT, EGG, DUAL
+
+    int growthDaysStd; // días estándar de crecimiento
+}
---
+++ src/main/java/com/tuempresa/chickenSuiteApp/modelo/Purpose.java
@@ -0,0 +1,7 @@
+package com.tuempresa.chickenSuiteApp.modelo;
+
+public enum Purpose {
+    MEAT,   // Carne
+    EGG,    // Huevos
+    DUAL    // Doble propósito
+}
